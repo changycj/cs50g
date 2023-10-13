fcf016b6df47a3f2ff32d0d61eb4281223054974
Yachi Chang <changycjudy@gmail.com>
2023-10-13 14:22:15 +0700

    Gem spawner

---
+++ Assets/Resources/Scripts/GemSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnGems());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnGems() {
+        while (true) {
+            Instantiate(prefabs[0], new Vector3(26, Random.Range(-10, 10), 10), Quaternion.identity);
+
+            yield return new WaitForSeconds(Random.Range(1, 5));
+        }
+    }
+}
