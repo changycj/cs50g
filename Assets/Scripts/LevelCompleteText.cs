0331f42f89ef3736fef832eed70119eeeafc0484
Yachi Chang <changycjudy@gmail.com>
2023-10-17 15:19:14 +0700

    assignment

---
+++ Assets/Scripts/LevelCompleteText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCompleteText : MonoBehaviour
+{
+    public static bool showText;
+    public Text levelCompleteText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        showText = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (showText) {
+            levelCompleteText.text = "LEVEL COMPLETE";
+        } else {
+            levelCompleteText.text = "";
+        }
+    }
+}
